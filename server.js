6133d742b391dfb4b4f8258941c18d1a98746afd
Boxydesign <petervincke@hotmail.com>
2024-10-15 21:58:24 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Route voor de hoofdpagina
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route voor credits
+app.get('/credits', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views_credits.html'));
+});
+
+// Route voor quorum
+app.get('/quorum', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views_quorum.html'));
+});
+
+// Route voor stemmen
+app.get('/stemmen', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views_stemmen.html'));
 });
 
 app.listen(PORT, () => {
